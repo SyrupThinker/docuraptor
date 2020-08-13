19d6d1afb94d6ecfb31b46b07a3b49bed8ff9684
Valentin Anger <syrupthinker@gryphno.de>
2020-08-13 20:35:54 +0200

    Upgrade std dependencies to v0.65.0 (Deno v1.3.0)

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
 export {
   assert,
   unreachable,
-} from "https://deno.land/std@0.61.0/testing/asserts.ts";
-export { parse as argsParse } from "https://deno.land/std@0.61.0/flags/mod.ts";
+} from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export { parse as argsParse } from "https://deno.land/std@0.65.0/flags/mod.ts";
 export {
   serve,
   ServerRequest,
-} from "https://deno.land/std@0.61.0/http/server.ts";
+} from "https://deno.land/std@0.65.0/http/server.ts";
